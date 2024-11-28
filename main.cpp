b7942dd984031e75a07ef47a8bef31abbb19438a
Karel Burda <38255062+karel-burda@users.noreply.github.com>
2024-11-28 15:59:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include "safe_env/safe_env.hpp"
 
+#include <caasert>
+
 using namespace burda;
 
+namespace
+{
 int test()
 {
-    env::set("foo", "value", 1);
+    env::setenv("foo", "value", 1);
+    assert(env::get_env("foo"), "value");
 }
+} // namespace anonymous
 
-int main([[maybe_unsed]] const int argc, [[maybe_unused]] const char** argv)
+int main([[maybe_unused]] const int argc, [[maybe_unused]] const char** argv)
 {
   return test();
 }
